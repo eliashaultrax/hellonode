f9db43b700b7b6d1d70f1f6a27ff8b6ef948a430
Elias Brunelli <elias.brunelli@haultrax.com>
2018-06-12 16:40:03 +0800

    Adjusts for DevOps
    Adjusting project to CI on Azure.

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-// load the http module
 var http = require('http');
 
-// configure our HTTP server
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com\n");
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  response.end("yahH It's working... i reckon :) \n");
 });
 
-// listen on localhost:8000
 server.listen(8000);
 console.log("Server listening at http://127.0.0.1:8000/");
